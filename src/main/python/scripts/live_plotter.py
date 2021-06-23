0398b2256b85ab7374933ef9d08bee222de27b41
kabilan <kavikabilan37@gmail.com>
2021-06-23 23:16:08 +0200

    updated live plot - adaptive window size, position marker, arguments of LiveFigure class

--- src/main/python/scripts/live_plotter.py
+++ src/main/python/scripts/live_plotter.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 from typing import *
-from matplotlib.backends.qt_compat import QtCore, QtWidgets
+from matplotlib.backends.qt_compat import QtCore
 from matplotlib.backends.backend_qt5agg import FigureCanvas
-import matplotlib as mpl
 import matplotlib.figure as mpl_fig
 import matplotlib.animation as anim
-import numpy as np
 
 class LiveFigure(FigureCanvas, anim.FuncAnimation):
     '''
@@ -14,46 +12,66 @@ class LiveFigure(FigureCanvas, anim.FuncAnimation):
     credits :  https://stackoverflow.com/questions/57891219/how-to-make-a-fast-matplotlib-live-plot-in-a-pyqt5-gui
 
     '''
-    def __init__(self, x_range:List, y_range:List, func, interval:int) -> None:
-        '''
-        :param x_len:       The nr of data points shown in one plot.
-        :param y_range:     Range on y-axis.
-        :param interval:    Get a new datapoint every .. milliseconds.
-
-        '''
+    def __init__(self, hedge) -> None:
+        ''' Initialization '''
         FigureCanvas.__init__(self, mpl_fig.Figure())
-        # Range settings
-        self._x_range_ = x_range
-        self._y_range_ = y_range
-
-        # Store two lists _x_ and _y_
-        self.mylen = 200
-        x = [0] * self.mylen
-        y = [0] * self.mylen
-
+        # Set buffersize
+        self.set_buffersize(20)
         # Store a figure and ax
         self._ax_  = self.figure.subplots()
         self._ax_.set_xlabel("X - axis")
         self._ax_.set_ylabel("Y - axis")
         self._ax_.axis('equal')
-        self._ax_.set_xlim(xmin=self._x_range_[0], xmax=self._x_range_[1])
-        self._ax_.set_ylim(ymin=self._y_range_[0], ymax=self._y_range_[1])
-        self._line_, = self._ax_.plot(x, y, linewidth = 2, color = 'black')
-
-        # Call superclass constructors
-        anim.FuncAnimation.__init__(self, self.figure, self._update_canvas_, fargs=(x,y,func,), interval=interval, blit=True)
+        # Path
+        self._line_, = self._ax_.plot(self.x, self.y, linewidth = 2, color = 'black')
+        # Position
+        self._point_ = self._ax_.scatter(self.x[-1], self.y[-1], marker='X', color = 'red', s= 100)
+        # Initialize animation
+        self.animation = anim.FuncAnimation(self.figure, self._update_canvas_, 
+                                            fargs=(self.x,self.y,hedge,), 
+                                            interval=20, blit=False)
+        self.paused = False
         return
     
-    def _update_canvas_(self, i,x, y,next_point_func) -> None:
-        '''
-        This function gets called regularly by the timer.
-
-        '''
-        current_x, current_y = next_point_func()
+    def _update_canvas_(self, i, x, y,hedge) -> None:
+        '''Function to update the elements of the canvas plot'''
+        position = hedge.position()
+        current_x = float(position[1]) # index of x
+        current_y = float(position[2]) # index of y
         x.append(round(current_x, 2))       # Add new datapoint
-        y.append(round(current_y, 2))       
+        y.append(round(current_y, 2))     
         x = x[-self.mylen:]                 # Truncate list _x_
         y = y[-self.mylen:]                 # Truncate list _y_
+        # update axis
+        margin = 2
+        self._ax_.set_xlim(min(x)-margin, max(x)+margin)
+        self._ax_.set_ylim(min(y)-margin, max(y)+margin)
+        # update path
         self._line_.set_xdata(x)
         self._line_.set_ydata(y)
-        return self._line_,
+        # update position
+        self._point_.remove()
+        self._point_ = self._ax_.scatter(x[-1], y[-1], marker='X', color = 'red', s= 100)
+        # return updates
+        return self._line_,self._point_ ,
+    
+    def set_buffersize(self, buffersize):
+        '''Function to set buffersize'''
+        self.mylen = int(buffersize)
+        # Store two lists _x_ and _y_
+        self.x = [0] * self.mylen
+        self.y = [0] * self.mylen
+
+    def toggle_live_visualization(self,state):
+        '''Function to pause or resume live visualization'''
+        if state == QtCore.Qt.Checked:
+            # Checked
+            if self.paused:
+                self.animation.resume() # resume animation
+                self.paused = not self.paused
+        else:
+            # Unchecked
+            if not self.paused:
+                self.animation.pause() # pause animation
+                self.paused = not self.paused
+

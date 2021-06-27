4ce4ff162553e9d53ab8345d9c31cb4a7734c9a1
karthik_sundararaj <skarthikbe96@gmail.com>
2021-06-27 12:39:48 +0200

    record option added

--- src/main/python/main.py
+++ src/main/python/main.py
@@ -34,6 +34,34 @@ class dummyhedge():
         return [id,x,y,z,angle,timestamp]
 hedge = dummyhedge() ######
 '''
+
+class Hedgerecord():
+    def get_datapoint(self):
+        print("started inside class")
+        current_date_and_time = datetime.datetime.now()
+        extension = ".txt"
+        file_name = f"src/main/python/trajectories/"+ str(current_date_and_time) + extension
+        self.f = open(file_name, "a")
+        while(True):
+            hedge.dataEvent.wait(1)
+            hedge.dataEvent.clear()
+            if (hedge.positionUpdated):
+                device_ID,x,y,z,angle,TimeStamp = hedge.position()  # BeaconID, X, Y, Z, Angle, Timestamp
+                valuelist = []
+                valuelist.append(device_ID)
+                valuelist.append(x)
+                valuelist.append(y)
+                valuelist.append(z)
+                valuelist.append(angle)
+                valuelist.append(TimeStamp)
+                self.f.write(str(valuelist))
+                self.f.write('\n')
+                # hedge.print_position()
+    def stop_datapoint(self):
+        hedge.stop() 
+        self.f.close() 
+        sys.exit()
+
 class AppContext(ApplicationContext):          
     def run(self):                              
         MainWindow = QMainWindow()
@@ -47,6 +75,14 @@ class AppContext(ApplicationContext):
         self.MyGUI.live_visualization_window.addWidget(self.myliveFig)
         self.MyGUI.live_visualization_enable.stateChanged.connect(self.myliveFig.toggle_live_visualization)
         self.MyGUI.live_buffersize_comboBox.activated[str].connect(self.myliveFig.set_buffersize)
+
+        Record = Hedgerecord()
+        
+        MyGUI.thread_1 = threading.Thread(target=Record.get_datapoint)
+        
+        MyGUI.record_recordstart_button.clicked.connect(lambda:self.recordstart(MyGUI,Record))
+        MyGUI.record_recordstop_button.clicked.connect(lambda:self.recordstop(MyGUI,Record))
+
         timer = QtCore.QTimer()
         timer.timeout.connect(self.update_livetab_GPSCoordinates)
         timer.start(500)
@@ -54,6 +90,17 @@ class AppContext(ApplicationContext):
         # Show
         MainWindow.show()
         return self.app.exec_()
+
+    def recordstart(self, MyGUI,Record):
+        print("thread started")
+        # MyGUI.thread_1.setDaemon(True)
+        MyGUI.thread_1.start()
+
+    def recordstop(self, MyGUI, Record):
+        print("thread stopped")
+        MyGUI.thread_1.exit()
+        Record.stop_datapoint()
+        # MyGUI.thread_1.setDaemon(False)
 
     def update_livetab_GPSCoordinates(self):
         '''Function to update GPS Coordinates display in Live tab'''

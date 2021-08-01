4e0479c42f2255df76fb5c5b662a58cf7b641899
Kabilan-T <kavikabilan37@gmail.com>
2021-08-01 20:06:35 +0200

    set and reset origin added

--- src/main.py
+++ src/main.py
@@ -10,12 +10,24 @@ from MarvelmindRobotics.src.marvelmind import MarvelmindHedge # Submodule provid
 # GUI window
 import GUI_Window as GUI # Created by PyQt5 UI code generator
 # scripts
-from scripts.live_plotter import LiveFigure 
+from scripts.live_plotter import LivePlotter
+from scripts.origin_plotter import OriginPlotter 
 
 # Creating MarvelmindHedge thread
 hedge = MarvelmindHedge(tty = "/dev/ttyACM0", adr=None, debug=False) # "/dev/ttyACM0" is serial port address
 hedge.daemon = True # kills thread when main program terminates
 hedge.start() # start thread
+
+Origin = [0.0, 0.0, 0.0]
+
+def set_origin(Origin,position,originFig):
+    Origin = [float(position[0]), float(position[1]), float(position[2])]
+    originFig.update_origin(Origin)
+
+def reset_origin(Origin,originFig):
+    Origin = [0.0, 0.0, 0.0]
+    originFig.update_origin(Origin)
+
 
 if __name__ == "__main__": 
     import sys
@@ -28,14 +40,18 @@ if __name__ == "__main__":
     # Interface here
 
     # Live Tab
-    myliveFig = LiveFigure(hedge,MyGUI)
-    MyGUI.live_visualization_window.addWidget(myliveFig)
-    MyGUI.live_visualization_enable.stateChanged.connect(myliveFig.toggle_live_visualization)
-    MyGUI.live_buffersize_comboBox.activated[str].connect(myliveFig.set_buffersize)
+    liveFig = LivePlotter(hedge,MyGUI)
+    MyGUI.live_visualization_window.addWidget(liveFig)
+    MyGUI.live_visualization_enable.stateChanged.connect(liveFig.toggle_live_visualization)
+    MyGUI.live_buffersize_comboBox.activated[str].connect(liveFig.set_buffersize)
     
     # Set Origin
-
-
+    
+    originFig = OriginPlotter(hedge,MyGUI, Origin)
+    MyGUI.setorigin_visualization_window.addWidget(originFig)
+    MyGUI.setorigin_visualization_enable.stateChanged.connect(originFig.toggle_live_visualization)
+    MyGUI.setorigin_setorigin_dialogbuttons.button(QtWidgets.QDialogButtonBox.Ok).clicked.connect(lambda: set_origin(Origin,hedge.position()[1:4],originFig))
+    MyGUI.setorigin_setorigin_dialogbuttons.button(QtWidgets.QDialogButtonBox.Reset).clicked.connect(lambda: reset_origin(Origin,originFig))
 
     # Record Tab
 
--- src/scripts/origin_plotter.py
+++ src/scripts/origin_plotter.py
@@ -6,19 +6,20 @@ from matplotlib.backends.backend_qt5agg import FigureCanvas
 import matplotlib.figure as mpl_fig
 import matplotlib.animation as anim
 
-class LivePlotter(FigureCanvas, anim.FuncAnimation):
+class OriginPlotter(FigureCanvas, anim.FuncAnimation):
     '''
     This is the LiveFigure in which the live plot is drawn.
     credits :  https://stackoverflow.com/questions/57891219/how-to-make-a-fast-matplotlib-live-plot-in-a-pyqt5-gui
 
     '''
-    def __init__(self, hedge, MyGUI) -> None:
+    def __init__(self, hedge, MyGUI, Origin) -> None:
         ''' Initialization '''
         FigureCanvas.__init__(self, mpl_fig.Figure())
         self.hedge = hedge
         self.MyGUI = MyGUI
+        self.Origin = Origin
         # Set buffersize
-        self.set_buffersize(20)
+        self.set_buffersize(500)
         # Store a figure and ax
         self._ax_  = self.figure.subplots()
         self._ax_.set_xlabel("X - axis")
@@ -26,9 +27,11 @@ class LivePlotter(FigureCanvas, anim.FuncAnimation):
         self._ax_.axis('equal')
         self._ax_.grid()
         # Path
-        self._line_, = self._ax_.plot(self.x, self.y, linewidth = 2, color = 'black')
+        self._line_, = self._ax_.plot(self.x, self.y, linewidth = 1, color = 'black')
         # Position
         self._point_ = self._ax_.scatter(self.x[-1], self.y[-1], marker='X', color = 'red', s= 100)
+        # Origin
+        self._origin_ = self._ax_.scatter(self.Origin[0], self.Origin[1], marker='P', color = 'green', s= 100)
         # Initialize animation
         self.animation = anim.FuncAnimation(self.figure, self._update_canvas_, 
                                             fargs=(self.x,self.y,), 
@@ -36,13 +39,20 @@ class LivePlotter(FigureCanvas, anim.FuncAnimation):
         self.paused = False
         return
     
+    def set_buffersize(self, buffersize):
+        '''Function to set buffersize'''
+        self.mylen = int(buffersize)
+        # Store two lists _x_ and _y_
+        self.x = [0] * self.mylen
+        self.y = [0] * self.mylen
+
     def _update_canvas_(self, i, x, y) -> None:
         '''Function to update the elements of the canvas plot'''
 
 
         position = self.hedge.position()
         current_x = float(position[1]) # index of x
-        current_y = float(position[2]) # index of y
+        current_y = float(position[2]) # index of y     
 
         x.append(round(current_x, 2))       # Add new datapoint
         y.append(round(current_y, 2))     
@@ -50,34 +60,47 @@ class LivePlotter(FigureCanvas, anim.FuncAnimation):
         y = y[-self.mylen:]                 # Truncate list _y_
         # update axis
         margin = 2
-        self._ax_.set_xlim(min(x)-margin, max(x)+margin)
-        self._ax_.set_ylim(min(y)-margin, max(y)+margin)
+        self._ax_.set_xlim(min(min(x),self.Origin[0])-margin, max(max(x),self.Origin[0])+margin)
+        self._ax_.set_ylim(min(min(y),self.Origin[1])-margin, max(max(y),self.Origin[1])+margin)
         # update path
         self._line_.set_xdata(x)
         self._line_.set_ydata(y)
         # update position
         self._point_.remove()
         self._point_ = self._ax_.scatter(x[-1], y[-1], marker='X', color = 'red', s= 100)
+        # update origin
+        self._origin_.remove()
+        self._origin_ = self._ax_.scatter(self.Origin[0], self.Origin[1], marker='P', color = 'green', s= 100)
 
         # labels
-        hedge_id = "Hedge ID : "+str(position[0])
+            # Origin
+        x_value = "X : "+str(self.Origin[0])
+        y_value = "Y : "+str(self.Origin[1])
+        z_value = "Z : "+str(self.Origin[2])
+        self.MyGUI.setorigin_origin_coordiantes_x.setText(x_value)
+        self.MyGUI.setorigin_origin_coordiantes_y.setText(y_value)
+        self.MyGUI.setorigin_origin_coordiantes_z.setText(z_value)
+            # Raw
         x_value = "X : "+str(position[1])
         y_value = "Y : "+str(position[2])
         z_value = "Z : "+str(position[3])
-        self.MyGUI.live_hedge_id.setText(hedge_id)
-        self.MyGUI.live_coordiantes_x.setText(x_value)
-        self.MyGUI.live_coordiantes_y.setText(y_value)
-        self.MyGUI.live_coordiantes_z.setText(z_value)
+        self.MyGUI.setorigin_rawcoordinates_x.setText(x_value)
+        self.MyGUI.setorigin_rawcoordinates_y.setText(y_value)
+        self.MyGUI.setorigin_rawcoordinates_z.setText(z_value)
+            # Transformed
+        x_value = "X : "+str(float(position[1])-float(self.Origin[0]))
+        y_value = "Y : "+str(float(position[2])-float(self.Origin[1]))
+        z_value = "Z : "+str(float(position[3])-float(self.Origin[2]))
+        self.MyGUI.setorigin_transformedcoordinates_x.setText(x_value)
+        self.MyGUI.setorigin_transformedcoordinates_y.setText(y_value)
+        self.MyGUI.setorigin_transformedcoordinates_z.setText(z_value)
 
         # return updates
         return self._line_,self._point_ ,
     
-    def set_buffersize(self, buffersize):
-        '''Function to set buffersize'''
-        self.mylen = int(buffersize)
-        # Store two lists _x_ and _y_
-        self.x = [0] * self.mylen
-        self.y = [0] * self.mylen
+    def update_origin(self, Origin):
+        '''Function to update origin position the plot'''
+        self.Origin = Origin
 
     def toggle_live_visualization(self,state):
         '''Function to pause or resume live visualization'''
